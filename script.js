cd6b148933d3760467a8b149015748395a1045da
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2022-03-23 13:33:47 -0500

    Javascript dates and form values finished

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('countdownForm');
+const dateEl = document.getElementById('date-picker');
+
+let countdownTitle = '';
+let countdownDate = '';
+
+// Set date input min with todays date
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min' , today);
+
+// Take values from form input
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+
+// Event listeners
+countdownForm.addEventListener('submit', updateCountdown);
